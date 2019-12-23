528ff24e102a8282036eec7191c61eb0d1cd94b0
tawargi <tawargi.m@gmail.com>
2019-12-24 00:11:44 +0200

    init

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-just test
+const header=document.querySelector('.header');
+const button=document.querySelector('.button');
+
+
+button.addEventListener('click',function () {
+	header.classList.toggle('toggle');
+})
